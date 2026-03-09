ae0d2b93f340676ba849e72dd2a9212e6834a4b1
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2026-03-09 15:35:35 -0300

    try page without dynamic params

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Link href="/1" prefetch>Page 1</Link>
-      <Link href="/2" prefetch>Page 2</Link>
+      <Link href="/posts" prefetch>Page link with prefetch enabled</Link>
     </div>
   );
 }
